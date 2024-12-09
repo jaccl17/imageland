8485c657a42d2904eb0df62e459c4619577aadcc
jack lawrence <jacclawrence@gmail.com>
2024-12-08 16:06:28 -0800

    removed unncessary file

--- DCT/view_prof.py
+++
@@ -1,5 +0,0 @@
-import pstats
-
-with open("profile_output.txt", "w") as f:
-    p = pstats.Stats('/home/unitx/wabbit_playground/DCT/profile_output.prof', stream=f)
-    p.sort_stats('cumulative').print_stats() 
